b272651d3c64e36ce24d1a4ac8f6cb5c196a42fb
Terminal_01 <steven484595@gmail.com>
2018-04-12 09:16:04 +0200

    [ADDED] All sales pictures

--- includes/weekactie.php
+++ includes/weekactie.php
@@ -19,7 +19,7 @@
   // display the data
   while($row = mysqli_fetch_row($result)){
     echo "<h3>".$row[0]."</h3>";
-    echo "<img src='../img/temptea.png'>"; // add $row[1] later
+    echo "<img src='../img/".$row[1]."'>";
     echo "<p>".$row[2]."</p>";
     echo "Deze actie geld tot: ".$row[3];
   }
--- index.php
+++ index.php
@@ -24,7 +24,7 @@
       <nav id="nav" class="navigation">
         <a href="index.php">Home</a>
         <a href="pages/productinfo.php">Productinfo</a>
-        <a href="pages/eventlijst.php">Eventlijst</a>
+        <a href="pages/eventlijst.php">Eventlijst</ a>
         <a href="pages/acties.php">Alle acties</a>
         <a href="javascript:void(0);" class="icon" onclick="opennav()">&#9776;</a>
       </nav>
